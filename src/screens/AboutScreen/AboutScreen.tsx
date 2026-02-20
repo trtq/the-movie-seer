235e9a18f6afd96990e7164e1e9986319a69e1d6
Евгений Иваницкий <evgenijivanickij@Evgenijs-MacBook-Pro.local>
2026-02-20 23:33:13 +0300

    about screen finished

--- src/screens/AboutScreen/AboutScreen.tsx
+++ src/screens/AboutScreen/AboutScreen.tsx
@@ -1,15 +1,67 @@
 import { TQuizNavigatorStackParamList, SCREENS } from "@/router/types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Container } from "./layouts";
-import { Button, Text } from "react-native";
+import {
+  DateText,
+  InfoButton,
+  InfoButtonIcon,
+  InfoButtonText,
+  InfoText,
+  InfoWrap,
+  NameText,
+  TextHighlight,
+  TMDBBlock,
+  TMDBLogo,
+  TMDBText,
+} from "./layouts";
+import { Linking } from "react-native";
+import { BackButton } from "@/components/BackButton/BackButton";
+import { SafeContainer } from "@/components/SafeContainer/SafeContainer";
 
 export const AboutScreen = ({
   navigation,
 }: NativeStackScreenProps<TQuizNavigatorStackParamList, SCREENS.About>) => {
   return (
-    <Container>
-      <Text>about</Text>
-      <Button title="back" onPress={navigation.goBack} />
-    </Container>
+    <SafeContainer>
+      <BackButton onPress={navigation.goBack} />
+      <InfoWrap>
+        <InfoText>
+          <TextHighlight>The Movie Seer</TextHighlight> is a game that tests
+          your knowledge of movies. Questions get generated automatically, so
+          you can play forever (even if some answers don&apos;t really make
+          sense).
+          {"\n"}Made in React Native.
+        </InfoText>
+        <InfoText>
+          Infromation about technologies used is available on my github.
+        </InfoText>
+        <InfoButton
+          onPress={() =>
+            Linking.openURL("https://github.com/trtq/the-movie-seer")
+          }
+        >
+          <InfoButtonText>
+            Source code <InfoButtonIcon name="github" />
+          </InfoButtonText>
+        </InfoButton>
+        <InfoButton
+          onPress={() =>
+            Linking.openURL("https://www.linkedin.com/in/evgeny-ivanitsky")
+          }
+        >
+          <InfoButtonText>
+            My LinkedIn <InfoButtonIcon name="linkedin-square" />
+          </InfoButtonText>
+        </InfoButton>
+        <NameText>© Evgeniy &quot;trtq&quot; Ivanitsky</NameText>
+        <DateText>February 2026</DateText>
+      </InfoWrap>
+      <TMDBBlock>
+        <TMDBLogo />
+        <TMDBText>
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb
+        </TMDBText>
+      </TMDBBlock>
+    </SafeContainer>
   );
 };
--- src/screens/AboutScreen/layouts.ts
+++ src/screens/AboutScreen/layouts.ts
@@ -1,8 +1,84 @@
 import styled from "styled-components/native";
+import { vs } from "react-native-size-matters";
+import { FontAwesome } from "@expo/vector-icons";
+import TMDBpic from "@assets/tmdb.webp";
+import { Image } from "expo-image";
 
-export const Container = styled.View`
+export const TMDBBlock = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: ${vs(10)}px ${vs(30)}px;
+`;
+
+export const TMDBLogo = styled(Image).attrs({
+  contentFit: "contain",
+  source: TMDBpic,
+})`
+  width: ${vs(60)}px;
+  height: ${vs(60)}px;
+`;
+
+export const TMDBText = styled.Text`
+  color: ${(props) => props.theme.primary};
+  font-size: ${vs(12)}px;
+  line-height: ${vs(15)}px;
   flex: 1;
-  background: white;
+  padding-left: ${vs(10)}px;
+  font-family: "NotoSans_400Regular";
+`;
+
+export const InfoWrap = styled.View`
+  flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 ${vs(30)}px;
+  align-self: stretch;
 `;
+
+export const InfoText = styled.Text`
+  color: ${(props) => props.theme.primary};
+  font-size: ${vs(13)}px;
+  line-height: ${vs(15)}px;
+  font-family: "NotoSans_400Regular";
+  width: 100%;
+  margin-bottom: ${vs(15)}px;
+`;
+
+export const TextHighlight = styled.Text`
+  font-family: "NotoSans_600SemiBold";
+  color: ${(props) => props.theme.highScore};
+`;
+
+export const InfoButton = styled.TouchableHighlight.attrs((props) => ({
+  underlayColor: props.theme.primaryTransparent,
+}))`
+  width: ${vs(180)}px;
+  height: ${vs(35)}px;
+  border-radius: ${vs(8)}px;
+  background: ${(props) => props.theme.menuButtonBackground};
+  border: ${vs(1)}px solid ${(props) => props.theme.menuButtonBorder};
+  justify-content: center;
+  align-items: center;
+  margin-bottom: ${vs(10)}px;
+`;
+
+export const InfoButtonText = styled.Text`
+  font-size: ${vs(13)}px;
+  color: ${(props) => props.theme.primary};
+`;
+
+export const NameText = styled.Text`
+  color: ${(props) => props.theme.primary};
+  font-size: ${vs(13)}px;
+  margin-bottom: ${vs(5)}px;
+`;
+
+export const DateText = styled.Text`
+  color: ${(props) => props.theme.primary};
+  font-size: ${vs(11)}px;
+`;
+
+export const InfoButtonIcon = styled(FontAwesome).attrs((props) => ({
+  color: props.theme.themeIconColor,
+  size: vs(10),
+}))``;
